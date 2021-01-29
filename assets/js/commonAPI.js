0a3e6d39b4a82d5057ea96430a4ab069e4d3084b
hww <836850740@qq.com>
2021-01-29 15:34:10 +0800

    完成首页渲染功能

--- assets/js/commonAPI.js
+++ assets/js/commonAPI.js
@@ -5,7 +5,14 @@ axios.defaults.baseURL = 'http://api-breakingnews-web.itheima.net'
 // 添加请求拦截器
 axios.interceptors.request.use(function(config) {
     console.log('---发送ajax请求前', config);
-    // 在发送请求之前做些什么
+    // 在发送请求之前判断是否有/my开头的请求路径
+    //如果有 手动添加headers请求头
+    console.log(config.url);
+    const token = localStorage.getItem('token') || ''
+    if (config.url.startsWith('/my')) {
+        config.headers.Authorization = token
+    }
+
     return config;
 }, function(error) {
     // 对请求错误做些什么
@@ -15,6 +22,17 @@ axios.interceptors.request.use(function(config) {
 // 添加响应拦截器
 axios.interceptors.response.use(function(response) {
     console.log('-----接收ajax响应前', response);
+    const { message, status } = response.data
+        //先判断身份验证是否失败
+    if (message == '身份验证失败!' && status == 1) {
+        //清除本地存储的token
+        localStorage.removeItem('token')
+            //跳转到登录页
+        location.href = './login.html'
+
+
+    }
+
     // 对响应数据做点什么
     return response.data;
 }, function(error) {
